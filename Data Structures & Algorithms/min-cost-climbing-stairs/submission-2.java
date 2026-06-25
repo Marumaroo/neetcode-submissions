b03d0d22402e4b40497f69e53632342388deb2eb
Hung Bui Quang <hungbuiquang2@gmail.com>
2026-06-25 19:56:54 +0100

    Add: min-cost-climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] memo = new int[n + 1];
+
+        for (int i = 2; i<= n; i++) {
+            memo[i] = Math.min(memo[i - 1] + cost[i - 1], memo[i-2] + cost[i-2]);
+        }
+
+        return memo[n];
+    }
+}
